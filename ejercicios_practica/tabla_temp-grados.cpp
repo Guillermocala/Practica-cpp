c7ab8704fd5ffcf845cd3a74269f59b8168edc59
Guillermo Cala <35546879+Guillermocala@users.noreply.github.com>
2018-10-18 11:56:48 -0500

    segunda version
    aqui el usuario ingresa los datos como un convertidor normal

--- ejercicios_practica/tabla_temp-grados.cpp
+++ ejercicios_practica/tabla_temp-grados.cpp
@@ -1,19 +1,20 @@
-/*crear un algoritmo que produzca una tabla de conversion de temperatura celsius a fahrenheit. 
-suponga que se van a desplegar temperaturas fahrenheit correspondientes a las temperaturas celsius 
-que varian de 5 a 50 grados en incrementos de cinco grados*/
+//crear un algoritmo que produzca una tabla de conversion de temperatura celsius a fahrenheit
 
 #include <iostream>
 
 using namespace std;
 
 int main(){
-	int grados_c = 5;
-	while(grados_c <= 50){
+	int grados_c;
+	cout << "\tCONVERTIDOS GRADOS CELSIUS A FAHRENHEIT\npara salir ingrese 0" << endl;
+	cout << "ingrese la cantidad de grados celsius: ";
+	cin >> grados_c;
+	while(grados_c != 0){
 		float grados_f = (grados_c * 1.8 ) + 32;
 		cout << "-" << grados_c << " grados celsius son: " << grados_f << " grados fahrenheit" << endl;
-		grados_c += 5;
+		cout << "ingrese la cantidad de grados celsius: ";
+		cin >> grados_c;
 	}
-	
 system("pause");
 return 0;
 }
