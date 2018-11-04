06fe9532b10939fa0f213ec39a4b60bceec32edf
Guillermocala <guillermo.cala7@gmail.com>
2018-11-03 23:51:09 -0500

    fun paso por referencia

--- ejercicios_practica/invertidor_texto.cpp
+++ ejercicios_practica/invertidor_texto.cpp
@@ -2,27 +2,30 @@
 #include <iostream>
 
 using namespace std;
-int cuentavector(char vector[], int *contador);
+int cuentavector(char vector[], int &contador);
+void imprimevector(char vector[], int contador);
 
 int main(){
-	char vector[100];
 	int cont = 0;
+	char vector[100];
 	cout << "\tINVERTIDOR DE TEXTO" << endl;
 	cout << "Ingrese la frase: ";
 	gets(vector);
-	cuentavector(vector, &cont);
-	cout << "La frase invertida es:";
-	for(int a=cont; a>=0; a--){
-		cout << vector[a];
-	}
+	cuentavector(vector, cont);
+	imprimevector(vector, cont);
 	cout << endl;
-	
 system("pause");
 return 0;
 }
-int cuentavector(char vector[], int *contador){
+int cuentavector(char vector[], int &contador){
 	for(int i=0; vector[i] != '\0'; i++){
-		*contador++;
+		contador++;
 	}
-	return *contador;
+	return contador;
+}
+void imprimevector(char vector[], int contador){
+	cout << "La frase invertida es:";
+	for(int a=contador; a>=0; a--){
+		cout << vector[a];
+	}
 }
