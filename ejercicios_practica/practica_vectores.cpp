53227d60daaeb86084b8e1286df21d355bd4fc6e
Guillermocala <guillermo.cala7@gmail.com>
2018-11-07 06:44:26 -0500

    2.1

--- ejercicios_practica/practica_vectores.cpp
+++ ejercicios_practica/practica_vectores.cpp
@@ -3,19 +3,12 @@
 using namespace std;
 
 int main(){
-	int cont;
-	char agregar;
-	char vocales[10];
-	cout << "cuantos datos va a ingresar?: ";
-	cin >> cont;
-	for(int a=0; a<cont; a++){
-		cout << "ingrese los datos: ";
-		cin >> agregar;
-		vocales[a] = agregar;
-	}
-	puts(vocales);
-	
-	cout << endl;
+	int numeros[10]={4,50,62,3,4,8};
+	int numero1=numeros[5];
+	cout << "el numero en la posicion 5 es: " << numero1 << endl;
+	numeros[5]=41;
+	int numero2=numeros[5];
+	cout << "el numero en la posicion 5 despues de modificar es: " << numero2 << endl;
 	system("pause");
 	return 0;
 }
