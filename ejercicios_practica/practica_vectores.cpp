54f4b135291c7b5422c203c0aa57855450db6164
Guillermocala <guillermo.cala7@gmail.com>
2018-11-07 05:40:41 -0500

    corrigiendo

--- ejercicios_practica/practica_vectores.cpp
+++ ejercicios_practica/practica_vectores.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(){
-	char vocales[5] = {'a','e','i','o','u'};
+	char vocales[6] = {'a','e','i','o','u','\0'};
 	int ancho = strlen(vocales);
-	for(int i=0; i<=ancho; i++){
-		cout << vocales[i];
+	for(int i=0; i<ancho; i++){
+		cout << vocales[i] << endl;
 	}
 	cout << endl;
 	system("pause");
