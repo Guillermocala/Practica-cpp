581ffea905299816a49edf26794f5054227a69d6
Guillermocala <guillermo.cala7@gmail.com>
2018-11-11 17:40:39 -0500

    estandares de programacion

--- ejercicios_practica/practica_vectores.cpp
+++ ejercicios_practica/practica_vectores.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 int main(){
-	int numeros[10]={4,50,62,3,4,8};
-	int numero1=numeros[5];
-	cout << "el numero en la posicion 5 es: " << numero1 << endl;
-	numeros[5]=41;
-	int numero2=numeros[5];
-	cout << "el numero en la posicion 5 despues de modificar es: " << numero2 << endl;
+	
 	system("pause");
 	return 0;
 }
