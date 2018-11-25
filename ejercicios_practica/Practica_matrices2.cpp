2bc730718b00e0a111b680dd1b42b2e09c8b0183
Guillermocala <guillermo.cala7@gmail.com>
2018-11-24 23:03:56 -0500

    usando funciones

--- ejercicios_practica/Practica_matrices2.cpp
+++ ejercicios_practica/Practica_matrices2.cpp
@@ -1,14 +1,16 @@
 /*elaborar un algoritmo que permita al usuario indicar las dimensiones del vector, leer los datos y luego imprimirlos*/
 #include <iostream>
 using namespace std;
+/*se usa el tipo de declaracion constante para manejar un espacio en memoria especifico 
+en vez de asignar valor constantes a cada campo*/
+const int CColumnas = 10; 
+const int CFilas = 10;
+int matriz[CFilas][CColumnas];
+void LeerMatriz (int matriz[CFilas][CColumnas], int NumFilas, int NumColumnas);
+void ImprimeMatriz (int matriz[CFilas][CColumnas], int NumFilas, int NumColumnas);
 
 int main()
 {
-	/*se usa el tipo de declaracion constante para manejar un espacio en memoria especifico 
-	en vez de asignar valor constantes a cada campo*/
-	const int CColumnas = 10; 
-	const int CFilas = 10;
-	int matriz[CFilas][CColumnas];
 	int filas, columnas;
 	cout << "Ingrese el numero de filas que va a tener el vector(maximo 10): ";
 	cin >> filas;
@@ -16,24 +18,9 @@ int main()
 	cin >> columnas;
 	if (filas > 0 && filas <= CFilas && columnas > 0 && columnas <= CColumnas)
 	{
-		for (int i = 0; i < filas; i++)
-		{
-			for (int j = 0; j < columnas; j++)
-			{
-				cout << "ingrese el elemento de la posicion [" << i + 1 << "] [" << j + 1 << "] : ";
-				cin >> matriz[i][j];
-			}
-		}
-	cout << "La matriz es la siguiente: " << endl;
-		for (int k= 0; k < filas; k++)
-		{
-			cout << "[";
-			for (int l = 0; l < 2; l++)
-			{
-				cout << "  " << matriz[k][l] << "  ";
-			}
-			cout << "] "<< endl;
-		}
+		LeerMatriz (matriz, filas, columnas);
+		cout << "La matriz es la siguiente: " << endl;
+		ImprimeMatriz (matriz, filas, columnas);
 	}
 	else
 	{
@@ -41,4 +28,27 @@ int main()
 	}
 	system("pause");
 	return 0;
+}
+void LeerMatriz (int matriz[CFilas][CColumnas], int NumFilas, int NumColumnas)
+{
+	for (int i = 0; i < NumFilas; i++)
+	{
+		for (int j = 0; j < NumColumnas; j++)
+		{
+			cout << "ingrese el elemento de la posicion [" << i + 1 << "] [" << j + 1 << "] : ";
+			cin >> matriz[i][j];
+		}
+	}
+}
+void ImprimeMatriz (int matriz[CFilas][CColumnas], int NumFilas, int NumColumnas)
+{
+	for (int k= 0; k < NumFilas; k++)
+	{
+		cout << "[";
+		for (int l = 0; l < NumColumnas; l++)
+		{
+			cout << "  " << matriz[k][l] << "  ";
+		}
+		cout << "] "<< endl;
+	}
 }
