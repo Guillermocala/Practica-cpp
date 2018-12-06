5280b306588dd737c65e806b030ba9699b8a3605
Guillermocala <guillermo.cala7@gmail.com>
2018-12-05 22:22:05 -0500

    corregido

--- Miscelanea_1/ejercicio17.cpp
+++ Miscelanea_1/ejercicio17.cpp
@@ -12,7 +12,7 @@ int main ()
 	const int CColumnas = 50;
 	int Numeros[CFilas][CColumnas];
 	int Numero = 1, NumFilas = 10, NumColumnas = 10, i, j;
-	for (i = 0; i < NumFilas; i++)
+	for (i = 0; i < NumFilas; i++)/*lectura de datos*/
 	{
 		for (j = 0; j < NumColumnas; j++)
 		{
@@ -20,17 +20,17 @@ int main ()
 			Numero++;
 		}
 	}
-	NumFilas++;
-	for (i = 0; i < NumFilas; i++)
+	for (i = 0; i < NumFilas; i++)/*suma de datos*/
 	{
-		float Suma = 0;
+		int Suma = 0;
 		for (j = 0; j < NumColumnas; j++)
 		{
-			Suma = Suma + Numeros[i][j]; 
+			Suma = Suma + Numeros[j][i];/*como se va a sumar las columnas el indice es al reves*/
 		}
-		Numeros[i][j] = Suma;
+		Numeros[j][i] = Suma;/*como se ingresa en la ultima fila, el inidice debe seguir siendo el mismo*/
 	}
-	for (i = 0; i < NumFilas; i++)
+	NumFilas++;
+	for (i = 0; i < NumFilas; i++)/*imprime datos*/
 	{
 		for (j = 0; j < NumColumnas; j++)
 		{
@@ -38,9 +38,33 @@ int main ()
 		}
 		cout << endl;
 	}
-	
-	
-	
 	system ("pause");
 	return 0;
 }
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+	int x, y, sum, numeros[11][10];
+	for (y = 0; y < 10; y++)
+	{
+		sum = 0;
+		for (x = 0; x < 10; x++)
+		{
+			numeros[x][y] = (x * 10) + 1 + y;
+			sum = sum + numeros[x][y];
+		}
+		numeros[10][y] = sum;
+	}
+	for (x = 0; x < 11; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			printf ("%d ", numeros[x][y]);
+		}
+		printf ("\n");
+	}
+	system ("PAUSE");     
+	return 0;
+}*/
