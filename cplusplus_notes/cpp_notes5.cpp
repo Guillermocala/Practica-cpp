1a2e8e24547f4088d9ce913afc09a07431eafca6
Guillermo <guillermo.cala7@gmail.com>
2019-02-02 01:21:54 -0500

    adding

--- cplusplus_notes/cpp_notes5.cpp
+++ cplusplus_notes/cpp_notes5.cpp
@@ -1,12 +1,16 @@
 /*
 *guillermo cala; 1/ feb/ 19
-*auto and decltype
+*auto and decltype and preprocessor definitions
 */
 #include <iostream>
 using namespace std;
+#define DEVELOPER "Guillermo"
+#define ENVIRONMENT "GNU/Linux"
 
 int main()
 {
+   cout << "the dev is: " << DEVELOPER << endl;
+   cout << "and he works in: " << ENVIRONMENT << endl;
    int num1;
    decltype(num1) num2; /*decltype when var is not initialized*/
    num1 = 5;
