7eb55733feadae64011b377a020f4378a0419216
Guillermo <guillermo.cala7@gmail.com>
2019-02-27 11:36:57 -0500

    corrected exercises

--- cplusplus_notes/cpp_notes10.cpp
+++ cplusplus_notes/cpp_notes10.cpp
@@ -1,5 +1,5 @@
 /*
-*guillermo cala; 26/ feb/ 19
+*guillermo cala; 26/ feb/ 19; mod 27/ feb/ 19
 *do while loop
 */
 #include "iostream"
@@ -7,10 +7,9 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-   int sw = 1;
+   int sw = 1, option;
    do
    {
-      system("clear");
       cout << "###############################" << endl;
       cout << "####           e.e         ####" << endl;
       cout << "###############################" << endl;
@@ -18,33 +17,32 @@ int main(int argc, char const *argv[])
       cout << "####     2- say goodbye    ####" << endl;
       cout << "####     3- exit           ####" << endl;
       cout << "###############################" << endl;
-      do
+      cout << "input a option: ";
+      cin >> option;
+      if (option >= 1 && option <=3)
       {
-         int opt;
-         cout << "input an option: ";
-         cin >> opt;
+         switch (option)
+         {
+            case 1:
+            cout << "hello n.n/" << endl;
+            cin.get();
+            break;
+            case 2:
+            cout << "goodbye :P" << endl;
+            cin.get();
+            break;
+            case 3:
+            cout << "your'e out" << endl;
+            cin.get();
+            sw = 0;
+            break;
+         }
       }
-      switch (opt)
+      else
       {
-         case 1:
-         cout << "hello n.n/" << endl;
-         cin.get();
-         break;
-         case 2:
-         cout << "goodbye :P" << endl;
-         cin.get();
-         break;
-         case 3:
-         cout << "your'e out" << endl;
-         cin.get();
+         cout << "you have entered incorrect data, the program will close..." << endl;
          sw = 0;
-         break;
-         default:
-         cout << "u input a wrong option" << endl;
-         cin.get();
-         break;
       }
-      while();
    }
    while(sw != 0);
    cin.get();
--- cplusplus_notes/cpp_notes11.cpp
+++ cplusplus_notes/cpp_notes11.cpp
@@ -1,5 +1,5 @@
 /*
-*guillermo cala; 26/ feb/ 19
+*guillermo cala; 26/ feb/ 19 ; mod 26/ feb/ 19
 *recursivity functions
 */
 #include "iostream"
@@ -9,6 +9,7 @@ void loop(int a, int b);
 int main(int argc, char const *argv[])
 {
    int lim, init;
+   cout << "this program will count the entry numbers in the sequence" << endl;
    cout << "the start of secuence must be less than or equal to the limit..." << endl;
    cout << "enter the start of sequence: ";
    cin >> init;
@@ -27,8 +28,8 @@ void loop(int a,int b)
       a++;
       loop(a, b);
    }
-   else
+   else if((a - 1) > b)
    {
-      /*cout << "you have entered incorrect data, the program will close..." << endl;*/
+      cout << "you have entered incorrect data, the program will close..." << endl;
    }
 }
