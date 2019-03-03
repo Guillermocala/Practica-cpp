cb897053948cb1c7dce1480f97055452b648f92e
Guillermo <guillermo.cala7@gmail.com>
2019-03-03 01:05:03 -0500

    corrigiendo

--- Miscelanea_1/ejercicio24.cpp
+++ Miscelanea_1/ejercicio24.cpp
@@ -15,19 +15,36 @@ struct producto
 	int precio;
 	int cantidad;
 } productos[10];
-void menu();
+int menu();
+void ImprimeList();
+void IngresaProd();
 
 int main()
 {
-	menu();
-
-
-
-
+	int sw = 1;
+	do
+	{
+		switch (menu())
+		{
+			case 1:
+				IngresaProd();
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				ImprimeList();
+				break;
+			case 0:
+				sw = 0;
+				break;
+		}
+	}while(sw != 0);
 	cin.get();
 	return 0;
 }
-void menu()
+int menu()
 {
 	system("clear");
 	cout << "\t###############################################" << endl;
@@ -40,18 +57,39 @@ void menu()
 	cout << "\t###############################################" << endl;
 	cout << "\t####   3- Modificar el producto            ####" << endl;
 	cout << "\t###############################################" << endl;
+	cout << "\t####   4- Mostrar listado de productos     ####" << endl;
+	cout << "\t###############################################" << endl;
+	cout << "\t####   0- Salir del programa               ####" << endl;
+	cout << "\t###############################################" << endl;
 	int opcion, sw = 1;
 	do
 	{
 		cout << "ingrese una opcion: ";
 		cin >> opcion;
-		if (opcion >= 1 && opcion <= 3)
+		if (opcion >= 0 && opcion <= 4)
 		{
 			sw = 0;
 		}
 		else
 		{
-			cout << "Debe ingresar un numero del 1 al 3..." << endl;
+			cout << "Debe ingresar un numero del 0 al 4..." << endl;
 		}
 	}while(sw != 0);
+	return opcion;
+}
+void IngresaProd()
+{
+	cout << "Ingrese el nombre: ";
+	cin >> productos[0].nombre;
+	cout << "Ingrese el precio: ";
+	cin >> productos[0].precio;
+	cout << "Ingrese la cantidad en stock: ";
+	cin >> productos[0].cantidad;
+}
+void ImprimeList()
+{
+	cout << productos[0].nombre << endl;
+	cout << productos[0].precio << endl;
+	cout << productos[0].cantidad << endl;
+
 }
