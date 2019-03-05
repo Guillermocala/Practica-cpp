8f10eb4dd1248d494c0c7706d6422537ca0d086b
Guillermo <guillermo.cala7@gmail.com>
2019-03-05 09:59:19 -0500

    completando las funciones basicas

--- Miscelanea_1/ejercicio25.cpp
+++ Miscelanea_1/ejercicio25.cpp
@@ -1,24 +1,27 @@
 /*
 *guillermo cala; march/ 04/ 19
-*elaborar un algoritmo que gestiona las notas de una clase de 20 alumnos(ni por el putas voy a hacer pruebas para 20, lo reduzco a 10) de los cuales sabemos el nombre y la nota. El programa debe ser capaz de:
+*elaborar un algoritmo que gestiona las nota de una clase de 20 alumnos(ni por el putas voy a hacer pruebas para 20, lo reduzco a 10) de los cuales sabemos el nombre y la nota. El programa debe ser capaz de:
 *- Buscar un alumno.
 *- Modificar su nota.
-*- Realizar la media de todas las notas.
-*- Realizar la media de las notas menores de 5.
-*- Mostrar el alumno que mejores notas ha sacado.
-*- Mostrar el alumno que peores notas ha sacado.
+*- Realizar la media de todas las nota.
+*- Realizar la media de las nota menores de 5.
+*- Mostrar el alumno que mejores nota ha sacado.
+*- Mostrar el alumno que peores nota ha sacado.
 */
 #include "iostream"
 #include "string"
 using namespace std;
 struct Estudiante{
    string nombre;
-   int notas[10];
+   int nota;
 } alum[10];
 
 int menu();
 void IngresaEst();
-void ImprimeList();
+void BuscarEst();
+void EditEst();
+void ImprimeEst();
+void ElimEst();
 int cont = -1;
 
 int main(int argc, char const *argv[])
@@ -37,36 +40,44 @@ int main(int argc, char const *argv[])
 			case 2:
 				system("clear");
 				cout << "\tBUSCAR UN ALUMNO(NOMBRE)" << endl;
-				void ImprimeList();
+				BuscarEst();
 				cin.get();
 				break;
 			case 3:
 				system("clear");
 				cout << "\tMODIFICAR DATOS DE ALUMNO" << endl;
+            EditEst();
 				cin.get();
-				break;
-			case 4:
+	         break;
+         case 4:
+            system("clear");
+            cout << "\tLISTA DE ESTUDIANTES" << endl;
+            ImprimeEst();
+            cin.get();
+            break;
+         case 5:
 				system("clear");
 				cout << "\tMEDIA DE TODAS LAS NOTAS" << endl;
 				cin.get();
 				break;
-			case 5:
+			case 6:
 				system("clear");
 				cout << "\tMEDIA DE NOTAS < 5" << endl;
 				break;
-			case 6:
+			case 7:
 				system("clear");
 				cout << "\tMEJOR ALUMNO" << endl;
 				cin.get();
 				break;
-			case 7:
+			case 8:
 				system("clear");
 				cout << "\tPEOR ALUMNO" << endl;
 				cin.get();
 				break;
-			case 8:
+			case 9:
 				system("clear");
 				cout << "\tELIMINAR ALUMNO(VACIAR SUS CAMPOS)" << endl;
+            ElimEst();
 				cin.get();
 				break;
 			case 0:
@@ -90,15 +101,17 @@ int menu()
 	cout << "\t#########################################################" << endl;
 	cout << "\t####   3- Modificar los datos de un alumno           ####" << endl;
 	cout << "\t#########################################################" << endl;
-	cout << "\t####   4- Media de todas las notas                   ####" << endl;
-	cout << "\t#########################################################" << endl;
-	cout << "\t####   5- Media de las notas menores que 5           ####" << endl;
-	cout << "\t#########################################################" << endl;
-   cout << "\t####   6- Alumno con las mejores notas               ####" << endl;
+   cout << "\t####   4- Listado de estudiantes                     ####" << endl;
    cout << "\t#########################################################" << endl;
-   cout << "\t####   7- Alumno con las peores notas                ####" << endl;
+	cout << "\t####   5- Media de todas las notas                   ####" << endl;
+	cout << "\t#########################################################" << endl;
+	cout << "\t####   6- Media de las notas menores que 5           ####" << endl;
+	cout << "\t#########################################################" << endl;
+   cout << "\t####   7- Alumno con las mejores notas               ####" << endl;
    cout << "\t#########################################################" << endl;
-   cout << "\t####   8- Eliminar un alumno                         ####" << endl;
+   cout << "\t####   8- Alumno con las peores notas                ####" << endl;
+   cout << "\t#########################################################" << endl;
+   cout << "\t####   9- Eliminar un alumno                         ####" << endl;
    cout << "\t#########################################################" << endl;
 	cout << "\t####   0- Salir del programa                         ####" << endl;
 	cout << "\t#########################################################" << endl;
@@ -107,7 +120,7 @@ int menu()
 	{
 		cout << "Ingrese una opcion: ";
 		cin >> opcion;
-		if(opcion >= 0 && opcion <= 8)
+		if(opcion >= 0 && opcion <= 9)
 		{
 			if(opcion == 1)
 			{
@@ -117,7 +130,7 @@ int menu()
 		}
 		else
 		{
-			cout << "Debe ingresar un numero del 0 al 8..." << endl;
+			cout << "Debe ingresar un numero del 0 al 9..." << endl;
 		}
 	}while(sw != 0);
 	return opcion;
@@ -130,21 +143,75 @@ void IngresaEst()
 		cout << "Ingrese el nombre: ";
 		cin.ignore();
 		getline (cin, alum[i].nombre);
+      cout << "Ingrese la nota: ";
+      cin >> alum[i].nota;
 		break;
 	}
 	if(cont >= 10)
 	{
 		cout << "No puede ingresar mas elementos. Solo 10!" << endl;
 	}
+}
+void BuscarEst()
+{
+	string indice;
+	cout << "Ingrese el nombre del estudiante a buscar: ";
+	cin >> indice;
+	for(int i = 0; i < 10; i++)
+	{
+		if(indice == alum[i].nombre)
+		{
+			cout << "Nota: " << alum[i].nota << endl;
+			break;
+		}
+		else
+		{
+			cout << "No se encontro el estudiante..." << endl;
+			break;
+		}
+	}
 	cin.get();
 }
-void ImprimeList()
+void EditEst()
+{
+	int indice;
+   char Confirm;
+   cout << "El indice del estudiante se encuentra en el listado" << endl;
+	cout << "Ingrese el indice del estudiante(numero): ";
+	cin >> indice;
+	indice -= 1;
+	cout << "Desea cambiar el nombre?(Y/N): ";
+   cin >> Confirm;
+   if (Confirm == 'Y' || Confirm == 'y')
+   {
+      cout << "Ingrese el nuevo nombre: ";
+      cin.ignore();
+      getline (cin, alum[indice].nombre);
+   }
+   else
+   {
+      cout << "Ingrese la nueva nota: " << endl;
+      cin >> alum[indice].nota;
+   }
+	cin.get();
+}
+void ImprimeEst()
 {
 	for(int i = 0; i < 10; i++)
 	{
 		cout << "\tESTUDIANTE N-" << i + 1 << endl;
 		cout << "Nombre: " << alum[i].nombre << endl ;
-		cout << "notas: " << alum[i].notas << endl;
+		cout << "Nota: " << alum[i].nota << endl;
 	}
 	cin.get();/*con esta pausa evitamos que se limpie pantalla antes de mostrar la info de este proc*/
+}
+void ElimEst()
+{
+	int indice;
+	cout << "El espacio del correspondiente producto quedara en blanco y para volver a usarlo debe usar la opcion 3(MODIFICAR)" << endl;
+	cout << "Ingrese el indice del estudiante: ";
+	cin >> indice;
+	indice -= 1;
+	alum[indice].nombre = "Unsigned";
+	alum[indice].nota = 0;
 }
