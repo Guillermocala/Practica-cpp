d63085ce41d2c4dc3a023cbde2733b5861796f97
Guillermo cala <gullermo.cala7@gmail.com>
2019-06-10 01:14:56 -0500

    haciendo algo

---
+++ cplusplus_notes/cpp_notes14.cpp
@@ -0,0 +1,44 @@
+/*
+*Guillermo Cala; 10/ 06/ 19
+*Arrays multidimensionales como arrays simples
+*/
+#include "iostream"
+using namespace std;
+void InsertData(int tabla[], int tope, int tope2);
+void ShowInfo(int tabla[], int tope, int tope2);
+int main()
+{
+   int Rows, Cols, Dim2;
+   cout << "Ingrese las filas: ";
+   cin >> Rows;
+   cout << "Ingrese las columnas: ";
+   cin >> Cols;
+   int numeros[Rows * Cols];
+   InsertData(numeros, Rows, Cols);
+   ShowInfo(numeros, Rows, Cols);
+   system("pause");
+   system("cls");
+   return 0;
+}
+void InsertData(int tabla[], int tope, int tope2)
+{
+   for(int i = 0; i < tope; i++)
+   {
+      for(int j = 0; j < tope2; j++)
+      {
+         cout << "Ingrese el dato: ";
+         cin >> tabla[i * tope2 + j];
+      }
+   }
+}
+void ShowInfo(int tabla[], int tope, int tope2)
+{
+   for(int i = 0; i < tope; i++)
+   {
+      for(int j = 0; j < tope2; j++)
+      {
+         cout << tabla[i * tope2 + j] << " ";
+      }
+      cout << endl;
+   }
+}
